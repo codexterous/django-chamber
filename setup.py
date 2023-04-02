656db906c046cba40a491a0e378e7e6eea4fca27
Brett Gmoser <bgmoser@codexterous.com>
2023-04-01 20:37:27 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Framework :: Django',
     ],
     install_requires=[
-        'django>=2.2, <4.0',
+        'django>=2.2, <4.2',
         'Unidecode>=1.1.1',
         'pyprind>=2.11.2',
         'filemagic>=1.6',
